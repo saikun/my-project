a1b83b6cb7c9d3384f47d8b8938c9596491770b7
hayakawa_yukio <hayakawa_yukio>
2019-09-19 12:03:53 +0900

    add Formschema1

--- src/router.ts
+++ src/router.ts
@@ -19,5 +19,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path:'/formschema1',
+      name:'formschema1',
+      component: () => import('./views/Formschema1.vue'),
+    }
   ],
 });
